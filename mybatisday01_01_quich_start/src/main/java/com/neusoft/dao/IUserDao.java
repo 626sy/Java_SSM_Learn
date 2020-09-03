0f623f2b99f818aef6a71f88937f672ab1bfb562
石浩博 <42900775+626sy@users.noreply.github.com>
2020-09-03 15:38:23 +0800

    测试

--- mybatisday01_01_quich_start/src/main/java/com/neusoft/dao/IUserDao.java
+++ mybatisday01_01_quich_start/src/main/java/com/neusoft/dao/IUserDao.java
@@ -1,10 +1,12 @@
 package com.neusoft.dao;
 
 import com.neusoft.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao {
     // 查询所有
+    @Select("select * from user")
     public List<User> findAll();
 }

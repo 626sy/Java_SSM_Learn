db42a9bd32c068c7cbe68d67b3e14ecea92d92aa
石浩博 <42900775+626sy@users.noreply.github.com>
2020-09-08 11:13:14 +0800

    重定向

--- springMVCday02_01_response/src/main/java/com/neusoft/controller/UserController.java
+++ springMVCday02_01_response/src/main/java/com/neusoft/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.neusoft.controller;
 
+import com.neusoft.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author shihaobo
@@ -13,8 +19,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     @RequestMapping("/testString")
     public String testString(Model model){
+        User user = new User();
+        // 假装么 这只调用service查出的结果
+        user.setUsername("龙岩");
+        user.setPassword("123");
+        user.setAge(16);
+        // model 对象会存储在request中
+        model.addAttribute("user",user);
+        return "success";
+    }
 
+    @RequestMapping("/testVoid")
+    public void testVoid(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("testVoid.......");
 
-        return "";
+        // 请求转发去响应
+//        request.getRequestDispatcher("/WEB-INF/pages/success.jsp").forward(request,response);
+        response.sendRedirect(request.getContextPath()+"/index.jsp");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        response.getWriter().write("你好啊");
+        return;
     }
 }
--- springMVCday02_01_response/target/springMVCday02_01_response/WEB-INF/pages/success.jsp
+++ springMVCday02_01_response/target/springMVCday02_01_response/WEB-INF/pages/success.jsp
@@ -5,13 +5,14 @@
   Time: 15:04
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
 <h1>入门成功，耶耶耶</h1>
+<%@ page isELIgnored="false" %>
 ${user.username}
 ${user.password}
 ${user.age}

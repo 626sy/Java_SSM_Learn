cb2c37ae77449e51ef29fe53526dcb6d5a4a4064
石浩博 <42900775+626sy@users.noreply.github.com>
2020-09-10 16:29:11 +0800

    增删改查

---
+++ Dtest/src/main/java/org/example/po/Emp.java
@@ -0,0 +1,96 @@
+package org.example.po;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+/**
+ * @author shihaobo
+ * @date 2020/9/10 14:21
+ */
+public class Emp {
+
+    private int empid;
+    private  String ename;
+    private Date hiredate;
+    private String logid;
+    private String logpwd;
+    private int mgrid;
+
+    public Emp() {
+    }
+
+    public Emp(int empid, String ename, Date hiredate, String logid, String logpwd, int mgrid) {
+        this.empid = empid;
+        this.ename = ename;
+        this.hiredate = hiredate;
+        this.logid = logid;
+        this.logpwd = logpwd;
+        this.mgrid = mgrid;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public String getLogid() {
+        return logid;
+    }
+
+    public void setLogid(String logid) {
+        this.logid = logid;
+    }
+
+    public String getLogpwd() {
+        return logpwd;
+    }
+
+    public void setLogpwd(String logpwd) {
+        this.logpwd = logpwd;
+    }
+
+    public int getMgrid() {
+        return mgrid;
+    }
+
+    public void setMgrid(int mgrid) {
+        this.mgrid = mgrid;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empid=" + empid +
+                ", ename='" + ename + '\'' +
+                ", hiredate=" + hiredate +
+                ", logid='" + logid + '\'' +
+                ", logpwd='" + logpwd + '\'' +
+                ", mgrid=" + mgrid +
+                '}';
+    }
+}
---
+++ Dtest/src/main/java/org/example/po/test.java
@@ -0,0 +1,92 @@
+package org.example.po;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author shihaobo
+ * @date 2020/9/10 15:01
+ */
+public class test {
+
+    private String resource = "mybatis-config.xml";
+    private InputStream inputStream;
+    private SqlSession sqlSession;
+
+    @Before
+    public void init() throws IOException {
+        inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession();
+    }
+    @After
+    public void destroy() throws IOException {
+        sqlSession.commit();
+        sqlSession.close();
+        inputStream.close();
+    }
+    // 查询一个
+//    @Test
+//    public void testSelectOne(){
+//        Emp e = sqlSession.selectOne("org.example.po.Emp.getEmp");
+//        System.out.println(e);
+//    }
+
+//    @Test
+//    public void testSelectAll(){
+//        List<Emp> list = sqlSession.selectList("org.example.po.Emp.getEmp2");
+////        for (Emp emp:list){
+////            System.out.println(emp);
+////        }
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//        System.out.println(list);
+//    }
+
+    @Test
+    public void testInsert(){
+        Emp emp = new Emp();
+        emp.setEname("liudehua");
+        emp.setLogid("ttc4");
+        int i = sqlSession.insert("org.example.po.Emp.addEmp",emp);
+        System.out.println(emp);
+        System.out.println("i"+i);
+    }
+
+    @Test
+    public void testUpdate(){
+        Emp emp = new Emp();
+        emp.setEmpid(11);
+        emp.setEname("zhangsan");
+        int i = sqlSession.update("org.example.po.Emp.updateEmp",emp);
+        System.out.println("i"+i);
+    }
+
+    @Test
+    public void delete(){
+        int delete = sqlSession.delete("org.example.po.Emp.removeEmp", 12);
+        System.out.println(delete);
+    }
+
+    @Test
+    public void testLike(){
+        Emp emp = new Emp();
+        emp.setEname("%张%");
+        List<Emp> list = sqlSession.selectList("org.example.po.Emp.likeEmp", emp);
+        System.out.println(list);
+        for (Emp e:list){
+            System.out.println(e);
+        }
+    }
+}
